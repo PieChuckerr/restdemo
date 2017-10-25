219569a55c735a6307843ea335ad04fd2338e07b
Govinda Sakhare <govindas@citiustech.com>
2017-10-25 18:44:17 +0530

    Unit cases added for message resource

--- src/main/java/com/example/restdemo/SwaggerConfig.java
+++ src/main/java/com/example/restdemo/SwaggerConfig.java
@@ -2,6 +2,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,6 +16,9 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.restdemo.web"))
+               /* .paths(PathSelectors.ant("/messages.*"))
+                .build();*/
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
